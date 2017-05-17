8888543391a84e57e89dbb525e1b165a8c75f737
ice1000 <ice1000kotlin@foxmail.com>
2017-05-17 17:27:11 +0800

    yeah I know how to add dependencies

--- lib/util/storage.dart
+++ lib/util/storage.dart
@@ -1,9 +1,16 @@
+import 'dart:async';
+import 'dart:io';
 
-
+import 'package:path_provider/path_provider.dart';
 
 class KeysAndValues {
   static const USER = "user_data";
   static const KATAS = "katas_data";
 }
 
-
+class Storage {
+  Future<File> _getLocalFile() async {
+    String dir = (await getApplicationDocumentsDirectory()).path;
+    return new File('$dir/counter.txt');
+  }
+}
--- lib/view/settings.dart
+++ lib/view/settings.dart
@@ -1,14 +1,22 @@
+import 'package:code_wars_android/code_wars/code_wars.dart';
 import 'package:code_wars_android/code_wars/colors.dart';
 import 'package:flutter/material.dart';
 
 class SettingsActivity extends MaterialPageRoute<Null> {
   static const String _title = "Settings";
+  CodeWarsUser _user;
 
   SettingsActivity() : super(builder: (BuildContext context) {
+    _user =
     return new Scaffold(
         appBar: new AppBar(title: new Text(_title)),
         backgroundColor: CodeWarsColors.black,
-
-    );
+        body: new ListView(primary: false, children: [
+          new ListTile(title: new Text("Change user name"),
+              subtitle: "",
+              trailing: new IconButton(
+                  icon: new Icon(Icons.edit), onPressed: () {
+              }))
+        ]));
   });
 }
